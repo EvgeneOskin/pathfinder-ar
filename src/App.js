dd44020c097414634a1de37ff6be5e277b4829bb
EvgeneOskin <eoskin@crystalnix.com>
2018-03-03 13:09:23 +0600

    Add custom marker and warrior image to this marker

--- src/App.js
+++ src/App.js
@@ -2,20 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { AFrameRenderer, Marker } from "react-web-ar";
+
 class App extends Component {
+  componentWillUnmount() {
+    this.scene.exitVR()
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <AFrameRenderer
+        arToolKit={{ sourceType: 'image', sourceUrl: '/images/warrior_marker.png'}}
+        stats
+        getSceneRef={ref => (this.scene = ref)}
+        inherent={true}
+      >
+        <Marker parameters={{ type: 'pattern', patternUrl: '/patterns/warrior.patt', preset: 'custom' }}>
+          <a-image height={0.5} front="front" src="/images/warrior.png">
+            <a-animation
+                attribute="rotation"
+                to="360 0 0"
+                dur="2000"
+                easing="linear"
+                repeat="indefinite"
+            />
+          </a-image>
+        </Marker>
+      </AFrameRenderer>
+    )
   }
 }
-
 export default App;
